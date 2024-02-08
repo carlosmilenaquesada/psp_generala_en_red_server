87eda3454c5ffe2b1bfd152f7774810b7ec0925c
carlosmilenaquesada <carlosmilenaquesada@gmail.com>
2024-02-08 18:10:28 +0000

    ultimo

--- src/modelos/flujo/serializaciones/SerializacionEstadoPartida.java
+++ src/modelos/flujo/serializaciones/SerializacionEstadoPartida.java
@@ -4,31 +4,35 @@ import java.io.Serializable;
 
 public class SerializacionEstadoPartida implements Serializable {
 
-    private int rondaTurnosActual;//de 1 a 12, doce turnos dura cada partida
+    private int rondaActual;//de 1 a 12, doce turnos dura cada partida
     private int tiradasRealizadasEnElTurnoDelJugador;//de 0 a 3 (empieza en cero porque puede que no haya hecho ninguna aún)
+    private int turnoDeLaRonda;//de 1 a 2 (1 primer jugador tira hasta 3 veces, 2 segundo jugador tira hasta 3 veces)
     private String idJugadorEnTurno;//la id del jugador al que le toca o le está tocando
 
-    public SerializacionEstadoPartida(int rondaTurnosActual, int tiradasRealizadasEnElTurnoDelJugador, String idJugadorEnTurno) {
-        this.rondaTurnosActual = rondaTurnosActual;
+    public SerializacionEstadoPartida(int rondaActual, int tiradasRealizadasEnElTurnoDelJugador, int turnoDeLaRonda, String idJugadorEnTurno) {
+        this.rondaActual = rondaActual;
         this.tiradasRealizadasEnElTurnoDelJugador = tiradasRealizadasEnElTurnoDelJugador;
+        this.turnoDeLaRonda = turnoDeLaRonda;
         this.idJugadorEnTurno = idJugadorEnTurno;
     }
+
+   
 
     //Constructor por defecto para cuando se recién se incie la partida (lo uso cuando
-    //ya conozco quien empezará)
+    //ya conozco quien empezará)   
     public SerializacionEstadoPartida(String idJugadorEnTurno) {
-        this.idJugadorEnTurno = idJugadorEnTurno;
-        this.rondaTurnosActual = 1;
+        this.rondaActual = 1;
         this.tiradasRealizadasEnElTurnoDelJugador = 0;
-
+        this.turnoDeLaRonda = 1;
+        this.idJugadorEnTurno = idJugadorEnTurno;
     }
 
-    public int getRondaTurnosActual() {
-        return rondaTurnosActual;
+    public int getRondaActual() {
+        return rondaActual;
     }
 
-    public void setRondaTurnosActual(int rondaTurnosActual) {
-        this.rondaTurnosActual = rondaTurnosActual;
+    public void setRondaActual(int rondaActual) {
+        this.rondaActual = rondaActual;
     }
 
     public int getTiradasRealizadasEnElTurnoDelJugador() {
@@ -39,6 +43,14 @@ public class SerializacionEstadoPartida implements Serializable {
         this.tiradasRealizadasEnElTurnoDelJugador = tiradasRealizadasEnElTurnoDelJugador;
     }
 
+    public int getTurnoDeLaRonda() {
+        return turnoDeLaRonda;
+    }
+
+    public void setTurnoDeLaRonda(int turnoDeLaRonda) {
+        this.turnoDeLaRonda = turnoDeLaRonda;
+    }
+
     public String getIdJugadorEnTurno() {
         return idJugadorEnTurno;
     }
@@ -47,9 +59,5 @@ public class SerializacionEstadoPartida implements Serializable {
         this.idJugadorEnTurno = idJugadorEnTurno;
     }
 
-    @Override
-    public String toString() {
-        return "SerializacionEstadoPartida{" + "rondaTurnosActual=" + rondaTurnosActual + ", tiradasRealizadasEnElTurnoDelJugador=" + tiradasRealizadasEnElTurnoDelJugador + ", idJugadorEnTurno=" + idJugadorEnTurno + '}';
-    }
 
 }
